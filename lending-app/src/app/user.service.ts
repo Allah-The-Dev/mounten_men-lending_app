66651ff282010c39d3dcf0ad5ae144b75d89e3a0
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-01-19 03:05:57 +0530

    sign up api call done

--- lending-app/src/app/user.service.ts
+++ lending-app/src/app/user.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
+import { LendingRestService } from './lending-rest.service';
+import { HttpClient, HttpHeaders, HttpResponse, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserDetails } from './models/user-details';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  apiUrl = "http://localhost:8080";
+  constructor(private httpClient : HttpClient) { }
 
-  createUser(result: any) {
+  createUser(userDetails: any):Observable<HttpResponse<any>> {
+    const signUpUrl = `{this.apiUrl}/user`
+    
+    return this.httpClient.post<any>(signUpUrl,userDetails,{
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      }),
+      observe: 'response'
+    });
     throw new Error("Method not implemented.");
   }
 }
