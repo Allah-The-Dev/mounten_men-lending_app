c149dedadaee8ea356c8e799ce6492b1159dc17f
Neelesh Pandey <neelesh.pandey@infosys.com>
2020-01-31 09:20:06 +0530

    home page done

---
+++ login-signup/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DefaultComponent } from './layouts/default/default.component';
+import { HomeComponent } from './modules/home/home.component';
+import { PostsComponent } from './modules/posts/posts.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'posts',
+        component: PostsComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports : [RouterModule]
+})
+export class AppRoutingModule { }
--- login-signup/src/app/app.module.ts
+++ login-signup/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { DefaultModule } from './layouts/default/default.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    DefaultModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
